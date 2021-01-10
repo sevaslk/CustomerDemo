b8fcf4513d5a3ae1aeabf15853cd3269d195201f
Vyacheslav Kirizleev <slava91274@gmail.com>
2021-01-10 13:12:02 +0300

    @Slf4j doesnt work

---
+++ src/main/resources/database/populateDB.sql
@@ -0,0 +1,2 @@
+-- INSERT INTO customers ('1', 'Ivan', 'Ivanov', 'Ukraine, Kiev, ul. Vesenniaia, 2', 10000);
+INSERT INTO customers (id, first_name, last_name, address, budget) VALUES (1, 'Ivan', 'Ivanov', 'Ukraine Kiev ul. Vesenniaia 2', 10000);
